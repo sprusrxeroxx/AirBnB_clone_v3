261680629775022016e9a3f39cd508561b047a0b
ntokozo vilakazi <ntokozovilakazi36@gmail.com>
2024-05-28 05:25:56 +0200

    chore: Add API endpoint for retrieving statistics

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,8 +5,17 @@ Creates a Flask app views
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Return status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Return stats"""
+    stats = {"Amenity": "amenities", "City": "cities", "Place": "places",
+               "Review": "reviews", "State": "states", "User": "users"}
+    return jsonify(stats)
