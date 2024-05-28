4156d96f4f4b4a08eb833956689361478783c5f9
ntokozo vilakazi <ntokozovilakazi36@gmail.com>
2024-05-28 04:23:33 +0200

    chore: Add get() and count() methods to FileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,46 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get(self):
+        """Test that get returns the object based on the class name and ID"""
+        storage = models.storage
+
+        storage.reload()
+
+        state_data = {"name":"Neverland"}
+
+        state_instance = State(**state_data)
+
+        retrieved_state = storage.get(State, state_instance.id)
+
+        self.assertEqual(state_instance,retrieved_state)
+
+        fake_state_id = storage.get(State, "fake_id")
+
+        self.assertEqual(fake_state_id, None)
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """Test that count returns the number of objects in storage"""
+        storage = FileStorage()
+        storage.reload()
+
+        state_data = {"name":"Congo"}
+        state_instance = State(**state_data)
+
+        storage.new(state_instance)
+
+        city_data = {"name":"Kinshasa", "state_id":state_instance.id}
+        city_instance = City(**city_data)
+
+        storage.new(city_instance)
+        storage.save()
+
+        state_occurrences = storage.count(State)
+        self.assertEqual(state_occurrences, len(storage.all(State)))
+
+        all_occurrences = storage.count()
+
+        self.assertEqual(all_occurrences, len(storage.all()))
